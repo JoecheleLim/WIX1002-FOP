46f08fd21b903050c5fd42ba4d7f60f2d50db643
JoecheleLim <limjoechele@gmail.com>
2025-05-26 11:25:38 +0800

    Lab 1

---
+++ L1Q4.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package l1q4;
+
+/**
+ *
+ * @author Lim Joechele
+ */
+public class L1Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Table 1: Total Sales of Product A");
+        System.out.println("____________________________");
+        System.out.println("|      Month      | Amount |");
+        System.out.println("____________________________");
+        System.out.println("|   January 2016  |  2500  |");
+        System.out.println("____________________________");
+        System.out.println("|  February 2016  |  1600  |");
+        System.out.println("____________________________");
+        System.out.println("|    March 2016   |  2000  |");
+        System.out.println("____________________________");
+        System.out.println("|    April 2016   |  2700  |");
+        System.out.println("____________________________");
+        System.out.println("|     May 2016    |  3200  |");
+        System.out.println("____________________________");
+        System.out.println("|     Jun 2016    |   800  |");
+        System.out.println("____________________________");
+        
+        System.out.println("\nBar chart");
+        System.out.println("   Month");
+        System.out.println("     ^");
+        System.out.println("JAN  |#########################");
+        System.out.println("FEB  |################");
+        System.out.println("MAR  |####################");
+        System.out.println("APR  |###########################");
+        System.out.println("MAY  |################################");
+        System.out.println("JUN  |########");
+        System.out.println("     |----|----|----|----|----|----|----|----|----> Amount (x10 * 2)");
+        System.out.println("     0    5    10   15   20   25   30   35  40");
+    }
+    
+    
+}
---
+++ MyProfile.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package l1q2;
+
+/**
+ *
+ * @author Lim Joechele
+ */
+public class MyProfile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Name       : Joechele Lim Qiu Ying");
+        System.out.println("Matric No. : 24004535");
+        System.out.println("Address    : KK10");
+        System.out.println("Email      : 24004535@siswa.um.edu.my");
+        System.out.println("Contact    : 014-300 1245");
+    }
+    
+}
